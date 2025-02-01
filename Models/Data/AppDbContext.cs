0ecc5637afad30b97b7a5f1f7e77d45c8751e723
maryshinyg <maryshiny16@gmail.com>
2025-02-01 13:10:24 +0530

    Add project files.

---
+++ Models/Data/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace NotesApp.Models.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<Note> Notes { get; set; }
+    }
+}
+
